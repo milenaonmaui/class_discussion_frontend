975d85527964edad85fecccf44e2fc5f91f7860e
Milena Kari <milenakari@yahoo.com>
2020-08-27 19:37:53 -1000

    add bootstrap material

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ class App extends Component {
       users: [],
       loading: false,
     }
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
   
   getUsers(){
@@ -18,21 +19,32 @@ class App extends Component {
     axios('https://randomuser.me/api/?nat=US&results=5')
     .then(res => 
       this.setState({
-        users: res.data.results,
+        users: [...this.state.users, ...res.data.results],
         loading: false
       }))
+  }
+
+  handleSubmit(e){
+    e.preventDefault()
+    this.getUsers()
   }
 
   componentDidMount(){
     this.getUsers()
   }
+
   render() {
-    return <div className="App">
-      {this.state.loading? <Loading msg="Loading message"/> : this.state.users.map(user => 
+    const {loading, users} = this.state;
+    return <div className="container">
+      {loading? <Loading msg="Loading message"/> : users.map(user => 
         <div key={user.cell}>
-          <h3>{user.name.first} {user.name.last}</h3>
+          <h3 style={{color: 'red'}}>{user.name.first} {user.name.last}</h3>
           <p>{user.email}</p>
+          
         </div>)}
+        <form onSubmit={this.handleSubmit}>
+            <input type="submit" value = "Load More Users"/>
+        </form>
       </div>
   };
 }
