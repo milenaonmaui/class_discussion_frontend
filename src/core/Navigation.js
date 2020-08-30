aab787b41213fd01e10967a5d67d57145b495117
Milena Kari <milenakari@yahoo.com>
2020-08-29 16:05:16 -1000

    Add signout method and link

--- src/core/Navigation.js
+++ src/core/Navigation.js
@@ -7,6 +7,22 @@ const isActive = (history, path) => {
     if(history.location.pathname === path) return {color: "#372196"}
     return {color: "white"}
 }
+
+export const signout = (next)=>{
+    if(typeof window !== "undefined"){
+        localStorage.removeItem("jwt");
+        next()
+        return fetch("http://localhost:8080/signout", {
+            method: "GET"
+        })
+        .then(response => {
+            console.log(response)
+            return response.json()
+        })
+        .catch(err =>console.log(err))
+    }
+} 
+
 const Navigation = ({history})=>(
     <div>
         <ul className="nav nav-tabs bg-primary">
@@ -18,6 +34,9 @@ const Navigation = ({history})=>(
             </li>
             <li className="nav-item">
                 <Link className="nav-link" style={isActive(history,"/signup")} to="/signup">Sign Up</Link>   
+            </li>
+            <li className="nav-item">
+                <button type="button" className="btn btn-link nav-link" onClick={() => signout(()=>history.push('/signin'))}>Sign Out</button>   
             </li>
         
         </ul>
